b1ab9b5559ed9a473110fe404e49496cf040e1e9
1_ruf <gyeol0411@naver.com>
2024-08-07 23:23:29 +0900

    MakeIntro
    I made menu scene intro dude

--- Assets/2.Scripts/HG/MenuScene/FIrstPopUp/StartUI.cs
+++ Assets/2.Scripts/HG/MenuScene/FIrstPopUp/StartUI.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class StartUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image gameLogo;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        gameLogo.DOFade(1, 0.5f);
+        StartCoroutine(LogoOut());
+    }
+    private IEnumerator LogoOut()
+    {
+        yield return new WaitForSeconds(1f);
+        gameLogo.DOFade(0, 1f);
     }
 }
